10a29f1ec685c60ab3c3b23316c705a0e4221ea2
Sayali Kale <skale@xpanxion.com>
2019-03-30 19:28:28 +0530

    jtestcommit

---
+++ src/main/java/Jenkins/jtest/loginpageoject.java
@@ -0,0 +1,40 @@
+package Jenkins.jtest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
+public class loginpageoject 
+{
+	
+	
+WebDriver driver;
+	
+public loginpageoject (WebDriver driver) // constructor
+{
+	this.driver = driver;
+}
+		
+	By email = By.id("email");
+  public WebElement email()
+	{
+		return driver.findElement(email);
+		
+	}
+	
+	By password = By.id("pass");
+	public WebElement password()
+	{
+		return driver.findElement(password);
+	 }
+	
+//	By sign = By.xpath("//*[@id=\"u_0_8\"]");
+//	public WebElement sign()
+//	{
+//		return driver.findElement(sign);
+//	 }
+//	
+}
---
+++ src/test/java/Jenkins/jtest/jetest.java
@@ -0,0 +1,45 @@
+
+package Jenkins.jtest;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+//import PageObjects.loginpage;
+//import PageObjects.loginpage;
+//import io.appium.java_client.android.AndroidDriver;
+//import io.appium.java_client.android.AndroidElement;
+import junit.framework.TestCase;
+//import junit.framework.Test;
+//import junit.framework.TestCase;
+//import junit.framework.TestSuite;
+
+public class jetest extends App
+{
+	WebDriver driver;
+	@Test
+	public void TC1() throws IOException
+    {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ksayali\\Downloads\\chromedriver.exe");
+     	WebDriver driver = new ChromeDriver();
+   		//driver1.manage().window().maximize();
+		
+       driver.get("https://www.facebook.com/"); //https://edgeconnect.ust-global.com/
+		//to access any method from any class need t create an Object of class and then call method of the class with an Object 
+		loginpageoject lp = new loginpageoject(driver);
+  		lp.email().sendKeys("sayalijkale@gail.com");
+	    lp.password().sendKeys("sjkherambha1891");
+	  //  lp.sign().click();
+	//	@Test
+//    public void testApp()
+//    {
+//		//https://www.facebook.com/
+//        System.out.println("Program Run");
+//    }
+}
+}
