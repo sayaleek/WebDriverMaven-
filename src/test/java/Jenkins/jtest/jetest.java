7cdfa344a49428a5178e54216cd953d546524db5
sayaleek <49095437+sayaleek@users.noreply.github.com>
2019-05-31 12:01:27 +0530

    Update jetest.java

--- src/test/java/Jenkins/jtest/jetest.java
+++ src/test/java/Jenkins/jtest/jetest.java
@@ -25,20 +25,20 @@ public class jetest extends App
 	@Test
 	public void TC1() throws IOException
     {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ksayali\\Downloads\\chromedriver.exe");
+		System.setProperty("");
      	WebDriver driver = new ChromeDriver();
    		//driver1.manage().window().maximize();
 		
-       driver.get("https://www.facebook.com/"); //https://edgeconnect.ust-global.com/
+       driver.get(""); 
 		//to access any method from any class need t create an Object of class and then call method of the class with an Object 
 		loginpageoject lp = new loginpageoject(driver);
-  		lp.email().sendKeys("sayalijkale@gail.com");
-	    lp.password().sendKeys("sjkherambha1891");
+  		lp.email().sendKeys("");
+	    lp.password().sendKeys("");
 	  //  lp.sign().click();
 	//	@Test
 //    public void testApp()
 //    {
-//		//https://www.facebook.com/
+//		
 //        System.out.println("Program Run");
 //    }
 	       
